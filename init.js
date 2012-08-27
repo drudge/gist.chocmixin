94df159e43dfb8fbaa26401364dacdf08368de95
Nicholas Penree <nick@penree.com>
2012-08-27 19:37:02 -0400

    Add notifications support

--- init.js
+++ init.js
@@ -9,7 +9,8 @@
  */
 
 var Gister = require('gister')
-  , keychain = require('keychain');
+  , keychain = require('keychain')
+  , exec = require('child_process').exec;
 
 /**
  * Service info.
@@ -19,6 +20,25 @@ var service = 'Chocolat Gist Plugin'
   , credentials = {}
   , self = this;
 
+
+function handleGistURL(url) {
+  if (!url) return;
+  
+  Clipboard.copy(url);
+  if (typeof Alert.notify !== 'undefined') {
+    Alert.notify({
+      title: "Public Gist Created",
+      subtitle: url,
+      body: "The URL has been copied to your clipboard.",
+      button: "Show",
+      callback: function () {
+        exec('/usr/bin/open "' + url + '"', function(err, stdout, stderr) {});
+      }
+    });
+  } else {
+    Alert.show('Public Gist Created', url, ['OK']);
+  }
+}
 /**
  * Create a public Gist from the current document.
  *
@@ -26,29 +46,39 @@ var service = 'Chocolat Gist Plugin'
  */
 
 function publicGistCurrentDocument() {    
-  if (!credentials.username || !credentials.password) {
+  if (!credentials.username) {
     showLoginWindow(publicGistCurrentDocument);
     return;
   }
 
-  var gist = new Gister({username: credentials.username, password: credentials.password })
-    , doc = Document.current()
-    , file = doc.filename()
-    , payload = {};
-  
-  file = file || 'untitled';
-  payload[file] = doc.text;
-  
-  gist.create(payload);
-  
-  gist.on('created', function (d) {
-    d = d || {};
-    url = d.html_url;
-    
-    if (url) {
-      Clipboard.copy(url);
-      Alert.show('Gist created', url, [ 'OK']);
+  keychain.getPassword({ account: credentials.username, service: service }, function(err, pass) {
+    if (pass) {
+      credentials.password = pass;
     }
+
+    if (!credentials.password) {
+      showLoginWindow(publicGistCurrentDocument);
+      return;
+    }
+
+    var gist = new Gister({username: credentials.username, password: credentials.password })
+      , doc = Document.current()
+      , file = doc.filename()
+      , payload = {};
+
+    file = file || 'untitled';
+    payload[file] = doc.text;
+
+    gist.create(payload);
+
+    gist.on('created', function (d) {
+      d = d || {};
+      url = d.html_url;
+
+      if (url) {
+        handleGistURL(url);
+      }
+    });
   });
 }
 
@@ -58,39 +88,54 @@ function publicGistCurrentDocument() {
  * @api public
  */
 
-function publicGistSelectedDocuments() {    
-  if (!credentials.username || !credentials.password) {
+function publicGistSelectedDocuments() {
+  if (!credentials.username) {
     showLoginWindow(publicGistSelectedDocuments);
     return;
   }
-  
-  var gist = new Gister({username: credentials.username, password: credentials.password })
-    , mainWindow = MainWindow.current()
-    , currentTab = (mainWindow ? mainWindow.currentTab() : null);
-  
-  if (currentTab) {
-    var docs = currentTab.visibleDocuments() || []
-      , payload = {}
-      , file;
-    
-    docs.forEach(function(doc) {
-      file = doc.filename() || 'untitled';
-      payload[file] = doc.text;
-    })
-    
-    gist.create(payload);
-    
-    gist.on('created', function (d) {
-      d = d || {};
-      url = d.html_url;
-      
-      if (url) {
-        Clipboard.copy(url);
-        Alert.show('Gist created', url, [ 'OK']);
-      }
-    });
-  }
+
+  keychain.getPassword({ account: credentials.username, service: service }, function(err, pass) {
+    if (pass) {
+      credentials.password = pass;
+    }
+
+    if (!credentials.password) {
+      showLoginWindow(publicGistSelectedDocuments);
+      return;
+    }
+
+    var gist = new Gister({username: credentials.username, password: credentials.password })
+      , mainWindow = MainWindow.current()
+      , currentTab = (mainWindow ? mainWindow.currentTab() : null);
+
+    if (currentTab) {
+      var docs = currentTab.visibleDocuments() || []
+        , payload = {}
+        , file;
+
+      docs.forEach(function(doc) {
+        file = doc.filename() || 'untitled';
+        payload[file] = doc.text;
+      })
+
+      gist.create(payload);
+
+      gist.on('created', function (d) {
+        d = d || {};
+        url = d.html_url;
+
+        if (url) {
+          handleGistURL(url);
+        }
+      });
+    }
+  });
 }
+
+/**
+ * Pull out github username if possible.
+ */
+credentials.username = Storage.persistent().get('githubUsername');
 
 /**
  * Hook up menu items.
@@ -107,19 +152,24 @@ Hooks.addMenuItem('Actions/Gist/Public Gist Selected Documents', 'command-contro
 
 function showLoginWindow(fn) {
   var win = new Window();
-  
+
   win.title = 'Login to Gist';
   win.useDefaultCSS = false;
   win.htmlPath = 'login.html';
   win.buttons = [ 'Login', 'Cancel' ];
   win.setFrame({x: 0, y: 0, width: 259, height: 211}, true);
-  
+
   win.onLoad = function() {
-    win.applyFunction(function (data) {
-      document.getElementById('username').focus();
-    }, []);
+    win.applyFunction(function (data) {      
+      if (data) {
+        document.getElementById('username').value = data;
+        document.getElementById('password').focus();
+      } else {
+        document.getElementById('username').focus();
+      }
+    }, [credentials.username]);
   };
-  
+
   win.onButtonClick = function(title) {
     if (title == 'Cancel') {
       win.close();
@@ -134,6 +184,8 @@ function showLoginWindow(fn) {
       Alert.show('Could not save your credentials.', 'Please fill in your username and password.', ['OK']);
       return;
     }
+    
+    Storage.persistent().set('githubUsername', user);
     
     keychain.setPassword(opts, function(err) {
       if (err) {
@@ -151,6 +203,6 @@ function showLoginWindow(fn) {
       }
     });
   };
-  
+
   win.run();
 }
